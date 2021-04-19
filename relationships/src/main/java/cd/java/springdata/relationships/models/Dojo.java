b3e5dd409a2b3cd52ea85ddcf07e2d5e18495b90
Caleb A Comstock <c_comstock@rocketmail.com>
2021-04-19 16:02:31 -0700

    created initial Dojo and Ninja model classes

---
+++ relationships/src/main/java/cd/java/springdata/relationships/models/Dojo.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package cd.java.springdata.relationships.models;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * @author ccomstock
+ *
+ */
+public class Dojo implements java.io.Serializable {
+    
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5573928576757699479L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(updatable=false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss a")
+    private LocalDateTime createdAt;
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss a")
+    private LocalDateTime updatedAt;
+    @OneToMany(mappedBy="dojo", fetch = FetchType.LAZY)
+    private List<Ninja> ninjas;
+    
+    public Dojo() {}
+    
+    public Dojo(String name) {
+    	this.name = name;
+    }
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @param createdAt the createdAt to set
+	 */
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * @return the updatedAt
+	 */
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	/**
+	 * @param updatedAt the updatedAt to set
+	 */
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	/**
+	 * @return the ninjas
+	 */
+	public List<Ninja> getNinjas() {
+		return ninjas;
+	}
+
+	/**
+	 * @param ninjas the ninjas to set
+	 */
+	public void setNinjas(List<Ninja> ninjas) {
+		this.ninjas = ninjas;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+    
+}
---
+++ relationships/src/main/java/cd/java/springdata/relationships/models/Ninja.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package cd.java.springdata.relationships.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * @author ccomstock
+ *
+ */
+@Entity
+@Table(name="ninjas")
+public class Ninja implements java.io.Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2542080832142145362L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    @Column(updatable=false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss a")
+    private LocalDateTime createdAt;
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss a")
+    private LocalDateTime updatedAt;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="dojo_id")
+    private Dojo dojo;
+    
+    public Ninja() {}
+    
+    public Ninja(String fn, String ln, Integer age) {
+    	firstName = fn;
+    	lastName = ln;
+    	this.age = age;
+    }
+    
+    /**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @param createdAt the createdAt to set
+	 */
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * @return the updatedAt
+	 */
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	/**
+	 * @param updatedAt the updatedAt to set
+	 */
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	/**
+	 * @return the Dojo
+	 */
+//	@JsonProperty
+	public Dojo getDojo() {
+		return dojo;
+	}
+
+	/**
+	 * @param dojo the Dojo to set
+	 */
+//	@JsonIgnore
+	public void setDojo(Dojo dojo) {
+		this.dojo = dojo;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+    
+}
